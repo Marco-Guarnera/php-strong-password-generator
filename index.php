fe9bedcb62120628f6352cfb3361e6227ad0c2d6
Marco-Guarnera <Darkaquon93@gmail.com>
2024-10-07 15:31:32 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+
+// Fase di preparazione
+
+// Variabili
+// Strutture dati
+// Funzioni
+
+// Fase di raccolta dati
+// Fase di elaborazione
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Meta -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Marco Guarnera">
+    <meta name="description" content="PHP Password Generator">
+    <!-- Title -->
+    <title>PHP Password Generator</title>
+    <!-- Bootstrap 5 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <!-- Main Header -->
+    <header id="main-header">
+        <h1 class="text-center">PHP Password Generator</h1>
+    </header>
+    <!-- Main -->
+    <main></main>
+</body>
+
+</html>
+
+<?php
+
+// Fase di produzione
+
+?>
