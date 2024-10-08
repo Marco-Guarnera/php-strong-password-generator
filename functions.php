a19f6ef05f4a14f9bd089916f3cb75e0ef8a0e4e
Marco-Guarnera <Darkaquon93@gmail.com>
2024-10-08 10:58:08 +0200

    Updated functions.php

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
 
 function getRandomPassword($password_length) {
     $random_password = '';
-    $set = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $set = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;=?@[\]^_{|}~';
     for ($i = 0; $i < $password_length; $i++) {
         $random_index = random_int(0, strlen($set) - 1);
         $random_password .= $set[$random_index];
